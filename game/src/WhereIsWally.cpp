6884104da72fd7377fa3f4a848b70af4f4f5a8fe
jramisDev <jesus.ramis.galindo@gmail.com>
2023-04-01 17:05:44 +0200

    PreparamosProyecto

---
+++ game/src/WhereIsWally.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************************
+*
+*   raylib game template
+*
+*   <Game title>
+*   <Game description>
+*
+*   This game has been created using raylib (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*
+*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include <iostream>
+
+//GameScreen currentScreen = LOGO;
+
+static const int screenWidth = 800;
+static const int screenHeight = 450;
+
+int main(void) {
+
+    std::cout << "Hello World!\n";
+
+    /* InitWindow(screenWidth, screenHeight, "raylib game template");
+
+     SetTargetFPS(60);
+
+     while (!WindowShouldClose()) {
+     }
+
+     CloseWindow();*/
+
+
+    return 0;
+}
